6ab721a7f84199356a0b0aa386232ce3a13330c6
NamedRoger <roge.villa.carrera@hotmail.com>
2021-03-06 00:59:10 -0600

    fix error

--- server/modules/classroom/services/periods/getPeriods.ts
+++ server/modules/classroom/services/periods/getPeriods.ts
@@ -2,7 +2,7 @@ import database from '../../../../database/database.ts';
 
 const table = "periodos";
 const query = `
-SELECT id_periodo, nombre, fecha_inicio, fecha_fin, actvio
+SELECT id_periodo, nombre, fecha_inicio, fecha_fin, activo
 FROM ${table}
 WHERE active = ${true}
 ORDER BY DESC
