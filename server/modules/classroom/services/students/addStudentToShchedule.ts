8368205f8069a803764eadcf128580a26812fd37
A-Damian11 <alandamian11.fr@gmail.com>
2021-03-08 15:34:56 -0600

    add

---
+++ server/modules/classroom/services/students/addStudentToShchedule.ts
@@ -0,0 +1,15 @@
+import database from '../../../../database/database.ts';
+import { StudentSchedule } from '../../models/StudentSchedule.ts';
+
+const table = "horario_alumno";
+
+const query = ({idSchedule,idStudent}:StudentSchedule) => `
+INSERT INTO ${table} (id_horario, id_alumno) 
+VALUES (${idSchedule},${idStudent})
+`;
+
+export default async (student:StudentSchedule) => {
+    const result = await database.execute(query(student));
+    if(result.affectedRows === 0) throw Error("No se insertó ningun registro");
+    return result;
+}
