8ec7d0cb932cac6d2d5a073bbc6e16e668bc25d5
NamedRoger <roge.villa.carrera@hotmail.com>
2021-03-09 16:33:27 -0600

    init user

---
+++ server/modules/auth/services/users/addUser.ts
@@ -0,0 +1,6 @@
+import database from '../../../../database/database.ts'
+import {User} from '../../models/index.ts';
+
+const query = ({idRole,name,username,active,curp,email,idUser, mLastname,matricula,pLastname,password}:User) => {
+
+}
