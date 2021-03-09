cb8a423d4c84d2552ace90615ae68395d3f5a05a
A-Damian11 <alandamian11.fr@gmail.com>
2021-03-08 19:09:32 -0600

    controllers

--- server/modules/classroom/controllers/classes/updateClass.ts
+++ server/modules/classroom/controllers/classes/updateClass.ts
@@ -1,14 +1,13 @@
-import {Schedule } from '../../models/schedule.ts';
-import { updateSchedule } from '../../services/schedule/scheduleService.ts';
+import {Class } from '../../services/class/classService.ts';
 
 export default async ({ request,response,params }: {request:any, response: any,params:any }) => {
     const body = await request.body({type:"json"});
     const data = await body.value;
     data.active = true;
-    data.idSchedule = params.idSchedule;
-    const schedule:Schedule = dataToSchedule(data);
+    data.idClass = params.idClass;
+    const Class:Class = dataToClass(data);
     try{
-        const res = await updateSchedule(schedule);
+        const res = await updateClass(Class);
         response.status = 204;
     }catch(e){
         response.body = {
@@ -17,8 +16,8 @@ export default async ({ request,response,params }: {request:any, response: any,p
     }
 }
 
-const dataToSchedule = (data:any):Schedule => ({
-    idSchedule:Number(data.idSchedule),
+const dataToClass = (data:any):Class => ({
+    idClass:Number(data.idClass),
     active:data.active,
     group:{
         idGroup:Number(data.idGroup),
