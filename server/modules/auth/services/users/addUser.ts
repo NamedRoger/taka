86df323de923c098ddf274878535c4b9f13a044b
A-Damian11 <alandamian11.fr@gmail.com>
2021-03-12 18:26:10 -0600

    add

--- server/modules/auth/services/users/addUser.ts
+++ server/modules/auth/services/users/addUser.ts
@@ -2,5 +2,5 @@ import database from '../../../../database/database.ts'
 import {User} from '../../models/index.ts';
 
 const query = ({idRole,name,username,active,curp,email,idUser, mLastname,matricula,pLastname,password}:User) => {
-
+    
 }
