046dbdee1cca3b98740abbc284cea273c2c30b07
A-Damian11 <alandamian11.fr@gmail.com>
2021-03-15 18:32:51 -0600

    users services

--- server/modules/auth/services/users/userServices.ts
+++ server/modules/auth/services/users/userServices.ts
@@ -2,7 +2,7 @@ import getUser from './getUsers.ts';
 import {getUserById} from './getUser.ts';
 import addUser from './addUser.ts';
 import updateUser from './updateUser.ts';
-import desactiveUser from './desactiveUser.ts';
+import desactiveUser from './desactiveUsers.ts';
 
 export {
     getUser,
