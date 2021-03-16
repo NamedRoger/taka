c11d4c5207c7d9dba9384d55059beea4baaa7c6d
NamedRoger <roge.villa.carrera@hotmail.com>
2021-03-15 19:59:15 -0600

    index models classroom

--- server/modules/classroom/controllers/periods/periodsController.ts
+++ server/modules/classroom/controllers/periods/periodsController.ts
@@ -2,11 +2,17 @@ import { IPeriod } from '../../models/period.ts';
 import * as periodsService from '../../services/periods/periodsService.ts';
 
 const getPeriods = async ({request,response}:{request:any,response:any}) => {
-
+    const periods = await periodsService.getPeriods();
+    response.body = periods;
 }
 
 const getPeriod = async ({request,response,params}:{request:any,response:any,params:any}) => {
-
+    const id = Number(params.idPeriod);
+    try{
+        const period = await periodsService.getPeriodById(id);
+        if(period === undefined || period === null) ;
+    }catch(e){
+    }
 }
 
 const addPeriod = async ({request,response}:{request:any,response:any}) => {
---
+++ server/modules/classroom/models/index.ts
@@ -0,0 +1,19 @@
+import {Class} from './class.ts';
+import {Group} from './group.ts';
+import {IPeriod} from './period.ts'
+import {Schedule} from './schedule.ts';
+import {Specialty} from './speciality.ts';
+import {StudentCalss} from './studentClass.ts';
+import {StudentSchedule} from './studentSchedule.ts'
+import {Topic} from './topic.ts';
+
+export type {
+    Class,
+    Group,
+    IPeriod,
+    Schedule,
+    Specialty,
+    StudentCalss,
+    StudentSchedule,
+    Topic
+}
